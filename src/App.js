7cfc5f61c1cfb47716935b2b5dff3c5d33b1bbed
crazzyrainbow <ankitkumarsuman6@gmail.com>
2023-10-06 07:53:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import F1 from './components/F1';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <F1 />
     </div>
   );
 }
 
 export default App;
+
+/*echo "# Test1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/crazzyrainbow/Test1.git
+git push -u origin main*/
